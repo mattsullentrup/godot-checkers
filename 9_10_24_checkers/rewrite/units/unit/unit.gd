f11583337e1b6924e478f9fddeb624e1b4041092
mattsullentrup <msullentrup1@gmail.com>
2024-09-15 20:55:51 -0500

    Setup jump tween animation

--- 9_10_24_checkers/rewrite/units/unit/unit.gd
+++ 9_10_24_checkers/rewrite/units/unit/unit.gd
@@ -13,16 +13,35 @@ var color: Color
 var can_move := false
 var tween: Tween
 
+@onready var _unit_visuals: UnitVisuals = %UnitVisuals
+
 
 func move(new_cell: Vector2i) -> void:
 	if tween:
 		tween.kill()
 	tween = create_tween()
+
 	var world_pos = Navigation.cell_to_world(new_cell)
-	tween.tween_property(self, "global_position", Vector2(world_pos), 1)
-	tween.tween_callback(_finish_moving.bind(new_cell))
+	tween.tween_property(self, "global_position", Vector2(world_pos), 1) \
+			.set_trans(Tween.TRANS_QUAD) \
+			.set_ease(Tween.EASE_IN_OUT)
+
+	tween.tween_interval(0.1)
+	#tween.tween_callback(_finish_moving.bind(new_cell))
+	await tween.finished
+	_finish_moving(new_cell)
 
 
 func _finish_moving(new_cell: Vector2i) -> void:
-	movement_completed.emit()
 	cell = new_cell
+	movement_completed.emit()
+
+
+func _jump_tween() -> void:
+	tween.set_parallel(true)
+	tween.tween_property(_unit_visuals, "radius", _unit_visuals.INITIAL_RADIUS + 30, 0.5) \
+			.set_trans(Tween.TRANS_SINE) \
+			.set_ease(Tween.EASE_OUT)
+	tween.tween_property(_unit_visuals, "radius", _unit_visuals.INITIAL_RADIUS, 0.5).set_delay(0.75) \
+			.set_trans(Tween.TRANS_SINE) \
+			.set_ease(Tween.EASE_IN_OUT)
--- 9_10_24_checkers/rewrite/units/unit/unit_visuals.gd
+++ 9_10_24_checkers/rewrite/units/unit/unit_visuals.gd
@@ -2,20 +2,22 @@ class_name UnitVisuals
 extends Node2D
 
 
-const RADIUS = 48
+const INITIAL_RADIUS = 48
 const WIDTH = 5
 @warning_ignore("integer_division")
 const OFFSET = Vector2i(Globals.CELL_SIZE / 2, Globals.CELL_SIZE / 2)
+
+var radius: float = INITIAL_RADIUS
 
 @onready var _parent: Unit = get_parent()
 
 
 func _draw() -> void:
 	var color: Color = get_parent().color
-	draw_circle(OFFSET, RADIUS, color, true, -1, true)
+	draw_circle(OFFSET, radius, color, true, -1, true)
 
 	if _parent.get_parent().selected_unit == _parent:
-		draw_circle(OFFSET, RADIUS, Color.YELLOW, false, WIDTH, true)
+		draw_circle(OFFSET, radius, Color.YELLOW, false, WIDTH, true)
 		if _parent.tween and _parent.tween.is_running():
 			return
 
@@ -24,10 +26,10 @@ func _draw() -> void:
 			var local_pos = Vector2i(to_local(world_pos))
 			draw_circle(
 					local_pos + OFFSET,
-					RADIUS, Color.AQUA, false, WIDTH, true
+					INITIAL_RADIUS, Color.AQUA, false, WIDTH, true
 			)
 	elif _parent.can_move:
-		draw_circle(OFFSET, RADIUS, Color.WHITE_SMOKE, false, WIDTH, true)
+		draw_circle(OFFSET, radius, Color.WHITE_SMOKE, false, WIDTH, true)
 
 
 func _process(_delta: float) -> void:
