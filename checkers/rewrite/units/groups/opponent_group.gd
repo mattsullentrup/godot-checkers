0a8b340e6e84ef094d9022ba592966f3c3fd092d
mattsullentrup <msullentrup1@gmail.com>
2024-12-13 19:09:40 -0600

    Put back jumped unit after evaluation

--- checkers/rewrite/units/groups/opponent_group.gd
+++ checkers/rewrite/units/groups/opponent_group.gd
@@ -36,9 +36,7 @@ func _minimax(board_state: Array, depth: int, is_maximizing: bool) -> int:
 	if depth == 0 or simulated_units["player"].is_empty() or simulated_units["enemy"].is_empty():
 		return get_board_evaluation(board_state)
 
-	# TODO: figure out how to simulate jumping over a piece
-
-	board_state = _duplicate_board_units(board_state.duplicate(true))
+	#board_state = _duplicate_board_units(board_state.duplicate(true))
 
 	var best = int(-INF) if is_maximizing else int(INF)
 	var group = self if is_maximizing else get_parent().get_node("PlayerGroup")
@@ -48,8 +46,16 @@ func _minimax(board_state: Array, depth: int, is_maximizing: bool) -> int:
 		for cell in unit.available_cells:
 			var original_cell = Navigation.world_to_cell(unit.position)
 			_simulate_move(cell, unit, board_state)
+			var jump_move := _get_simulated_jump(cell, unit, board_state)
+
 			eval = _minimax(board_state, depth - 1, not is_maximizing)
+
+			# Undo previous move and put back jumped unit if the move was a jump
 			_simulate_move(original_cell, unit, board_state)
+			if jump_move:
+				var jumped_unit = jump_move.jumpable_unit
+				var jumped_cell = Navigation.world_to_cell(jumped_unit.position)
+				board_state[jumped_cell.y][jumped_cell.x] = jumped_unit
 
 			if is_maximizing:
 				if eval > best:
@@ -86,8 +92,9 @@ func _simulate_move(new_cell: Vector2i, unit: Unit, board_state: Array[Array]) -
 	board_state[unit_cell.y][unit_cell.x] = null
 	board_state[new_cell.y][new_cell.x] = unit
 	unit.position = Navigation.cell_to_world(new_cell)
-	#if unit.jump_paths.any(func(path: Array): \
-			#return path.any(func(j: JumpData): return j.target_cell == new_cell)):
+
+
+func _get_simulated_jump(new_cell: Vector2i, unit: Unit, board_state: Array[Array]) -> JumpData:
 	var data: JumpData
 	for path in unit.jump_paths:
 		for jump_data in path:
@@ -101,9 +108,8 @@ func _simulate_move(new_cell: Vector2i, unit: Unit, board_state: Array[Array]) -
 			if row.has(jumped_unit):
 				var jumped_cell = Navigation.world_to_cell(jumped_unit.position)
 				board_state[jumped_cell.y][jumped_cell.x] = null
-				break
-
-		#jumped_unit.free()
+				return data
+	return null
 
 
 func _duplicate_board_units(board_state: Array[Array]) -> Array[Array]:
